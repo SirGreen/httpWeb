195b9c0edaaaa187af23ac005e29d323a8bf0529
Huỳnh Minh Khoa <61875709+SirGreen@users.noreply.github.com>
2024-11-20 23:09:29 +0700

    update app

--- app.py
+++ app.py
@@ -2,20 +2,29 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('login.html')
+    return render_template("survey.html")
 
-@app.route('/login', methods=['POST'])
+
+@app.route("/login", methods=["POST"])
 def login():
-    account = request.form.get('account')
-    password = request.form.get('password')
+    account = request.form.get("account")
+    password = request.form.get("password")
     # Normally, you would validate the account and password here, but we're skipping this for the demo.
-    return redirect(url_for('login_success'))
+    return redirect(url_for("login_success"))
 
-@app.route('/login-success')
+
+@app.route("/login-success")
 def login_success():
     return "<h1>Login Successful</h1>"
 
-if __name__ == '__main__':
+
+@app.route("/submit-survey", methods=["POST"])
+def submit_survey():
+    return "<h1>Thank you for your time!</h1>"
+
+
+if __name__ == "__main__":
     app.run(debug=True)
